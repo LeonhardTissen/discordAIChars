6f689c4c6d29ef0153227065d70e1630e998cb17
Leonhard Tissen <contact@warze.org>
2024-04-22 15:26:47 +0200

    Cleanup: Refactor webhook.js for better readability

--- src/webhook.js
+++ src/webhook.js
@@ -10,31 +10,32 @@ export let currentWebhookModel = {
 
 const webhookFilePath = 'webhook.json';
 
+async function createWebhook() {
+	const name = Math.random().toString(36).substring(7);
+	webhook = await channel.createWebhook({name});
+	const { id, token } = webhook;
+	
+	fs.writeFileSync(webhookFilePath, JSON.stringify({ id, token, avatar: null, name }));
+
+	console.log(`Webhook created: ${webhook.id}`);
+}
+
+async function loadWebhook() {
+	const webhookFile = fs.readFileSync(webhookFilePath, 'utf8');
+	const webhookData = JSON.parse(webhookFile);
+	const { id, token, avatar, displayName } = webhookData;
+	
+	webhook = new WebhookClient({ id, token });
+	currentWebhookModel.displayName = displayName;
+	currentWebhookModel.avatar = avatar;
+
+	console.log(`Webhook loaded: ${webhook.id}`);
+}
+
 export async function getOrCreateWebhook() {
 	if (fs.existsSync(webhookFilePath)) {
-		// Load webhook from file
-		const webhookFile = fs.readFileSync(webhookFilePath, 'utf8');
-		const webhookData = JSON.parse(webhookFile);
-		webhook = new WebhookClient({
-			id: webhookData.id,
-			token: webhookData.token
-		});
-		currentWebhookModel.displayName = webhookData.displayName;
-		currentWebhookModel.avatar = webhookData.avatar;
-
-		console.log(`Webhook loaded: ${webhook.id}`);
+		loadWebhook();
 	} else {
-		// Create webhook
-		const name = Math.random().toString(36).substring(7);
-		webhook = await channel.createWebhook({name});
-		
-		fs.writeFileSync(webhookFilePath, JSON.stringify({
-			id: webhook.id,
-			token: webhook.token,
-			avatar: null,
-			name
-		}));
-
-		console.log(`Webhook created: ${webhook.id}`);
+		createWebhook();
 	}
 }
