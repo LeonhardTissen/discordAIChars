ac973e8f7242171a75101101341eed145b1b152f
Leonhard Tissen <contact@warze.org>
2024-04-22 12:02:43 +0200

    Cleanup: Use DB functions, always use idName

--- commands/ask.js
+++ commands/ask.js
@@ -2,8 +2,8 @@ import { talkToModel } from "../src/ollama.js";
 
 export function cmdAsk(restOfMessage) {
 	// Example: !ask Ben What is the capital of France?
-	const [modelName, ...prompt] = restOfMessage.split(' ');
+	const [idName, ...prompt] = restOfMessage.split(' ');
 	const promptString = prompt.join(' ');
 
-	talkToModel(promptString, modelName);
+	talkToModel(promptString, idName);
 }
--- commands/avatar.js
+++ commands/avatar.js
@@ -1,35 +1,26 @@
-import { db } from "../src/db.js";
+import { db, getModel } from "../src/db.js";
 
 export async function cmdAvatar(restOfMessage, message) {
-	const [modelName, avatar] = restOfMessage.split(' ');
+	const [idName, avatar] = restOfMessage.split(' ');
 
-	if (!modelName || !avatar) return '### Please specify a model and an avatar URL'
+	if (!idName || !avatar) return '### Please specify a model and an avatar URL'
 
 	// Check if owner
-	const modelData = await new Promise((resolve, reject) => {
-		db.get('SELECT owner FROM models WHERE idname = ?', [modelName], (err, row) => {
-			if (err) {
-				reject(err);
-				return;
-			}
-			resolve(row);
-		}
-	)});
+	const modelData = await getModel(idName);
 
-	if (!modelData) return `### Model with name "${modelName}" not found`
+	if (!modelData) return `### Model with name "${idName}" not found`
 
 	const { owner } = modelData;
 
-	if (owner !== message.author.id) return `### You do not own the model with the name "${modelName}"`
+	if (owner !== message.author.id) return `### You do not own the model with the name "${idName}"`
 
 	// Edit avatar
-	db.run('UPDATE models SET profile = ? WHERE idname = ?', [avatar, modelName], async (err) => {
+	db.run('UPDATE models SET profile = ? WHERE idname = ?', [avatar, idName], async (err) => {
 		if (err) {
 			console.error(err);
 			return;
 		}
-
 	});
 	
-	return `### Avatar for model "${modelName}" updated`
+	return `### Avatar for model "${idName}" updated`
 }
