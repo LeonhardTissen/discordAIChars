3918ceba80f42687f67340fac0239311b708edc3
Leonhard Tissen <contact@warze.org>
2024-04-23 17:41:08 +0200

    Fix: Properly return response if invalid default model

--- commands/default.js
+++ commands/default.js
@@ -17,10 +17,7 @@ async function cmdDefault(idName) {
 
 	const modelData = await getModel(idName);
 
-	if (!modelData) {
-		await message.channel.send(`### Model with name "${idName}" not found`);
-		return;
-	}
+	if (!modelData) return `### Model with name "${idName}" not found`
 
 	setDefaultChannelModel(idName);
 	return `### Default model set to "${idName}"\nThis means you can talk to this model without running the !ask command.`
