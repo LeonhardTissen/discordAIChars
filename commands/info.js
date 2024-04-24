a7d8d7f4668e0a897d705aa38ec1c97ec1c26a8a
Leonhard Tissen <contact@warze.org>
2024-04-24 23:23:43 +0200

    Fix: Object destructuring before existence verification

--- commands/info.js
+++ commands/info.js
@@ -11,10 +11,10 @@ async function cmdInfo(idName) {
 	if (!idName) return '### Please specify a model to show info'
 
 	const row = await getModel(idName);
-
-	const { displayname, owner, model, profile } = row;
-
+	
 	if (!row) return `### Model with name "${idName}" not found`
+	
+	const { displayname, owner, model, profile } = row;
 
 	return `### Model info for "${displayname}":
 - Avatar: ${profile}
