79d611b31cd0f407af0a0bc978f0fc765931224f
Leonhard Tissen <contact@warze.org>
2024-04-29 10:54:47 +0200

    Feature: Added ability to search for commands in help

--- commands/help.js
+++ commands/help.js
@@ -3,10 +3,11 @@ import { registerCommand, commands, categoryNames } from "../src/registrar.js";
 
 /**
  * Display help text.
+ * @param {string} searchTerm - The command to get help for 
  * @returns {string} - The response message
  * @example !help
  */
-function cmdHelp() {
+function cmdHelp(searchTerm) {
 	const helpCategories = {};
 
 	// Group commands together by category
@@ -14,12 +15,20 @@ function cmdHelp() {
 		// Skip debug commands
 		if (category === 'Debug') return;
 
+		// Skip commands that don't match the search term
+		if (searchTerm && !command.includes(searchTerm)) return;
+
 		if (!helpCategories[category]) {
 			helpCategories[category] = [];
 		}
 
 		helpCategories[category].push({ command, description, parameters });
 	});
+
+	// No commands found
+	if (Object.keys(helpCategories).length === 0) {
+		return '### No commands found';
+	}
 
 	let helpText = Object.entries(helpCategories).map(([category, commands]) => {
 		const allCommandsInCategory = commands.map(({ command, description, parameters }) => {
@@ -33,7 +42,9 @@ function cmdHelp() {
 		return `$t${categoryNames[category]}:\n${allCommandsInCategory}\n`;
 	}).join('\n');
 	
-	helpText += '\n$pMessages starting with # or _ will be ignored by the AI. It will also ignore any messages while a message is being generated.';
+	if (!searchTerm) {
+		helpText += '\n$pMessages starting with # or _ will be ignored by the AI. It will also ignore any messages while a message is being generated.';
+	}
 
 	return format(helpText);
 }
