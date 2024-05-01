6a895c86dbcca9a38c5eb319a3d4b407f0b5177e
Leonhard Tissen <contact@warze.org>
2024-05-01 20:47:34 +0200

    Feature: Proper error handling of missing arguments in !ask commands

--- commands/ask.js
+++ commands/ask.js
@@ -4,11 +4,17 @@ import { talkToModel } from "../src/ollama.js";
 /**
  * Ask the model a question directly.
  * @param {string} restOfMessage - The message after the command.
+ * @returns {string} - The response to the command.
  * @example !ask Ben How are you?
+ * 
  */
 function cmdAsk(restOfMessage) {
 	const [idName, ...prompt] = restOfMessage.split(' ');
 	const promptString = prompt.join(' ');
+
+	if (!idName) return '### Please provide a name for the model.';
+
+	if (!promptString) return '### Please provide a prompt for the model to answer.';
 
 	talkToModel(promptString, idName);
 }
