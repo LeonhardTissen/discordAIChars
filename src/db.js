a57d9ff89dcd71f0c41a872b5662a95ea270a107
Leonhard Tissen <contact@warze.org>
2024-05-08 21:09:43 +0200

    Cleanup: New runQuery function to save on duplicate code

--- src/db.js
+++ src/db.js
@@ -7,74 +7,49 @@ db.serialize(() => {
 	db.run('CREATE TABLE IF NOT EXISTS models (idname TEXT UNIQUE, displayname, model TEXT, owner TEXT, profile TEXT)');
 });
 
-export async function getAllModels() {
+/*
+ * Run a query on the database
+ * @param {string} verb - The type of query to run (get, all, run)
+ * @param {string} query - The SQL query to run
+ * @param {Array} params - The parameters to pass to the query
+ * @returns {Promise} - A promise that resolves with the result of the query
+ * @throws {Error} - If an invalid verb is passed
+ * @private
+ */
+function runQuery(verb, query, params = []) {
+	if (![ 'get', 'all', 'run' ].includes(verb)) throw new Error('Invalid verb');
+
 	return new Promise((resolve, reject) => {
-		db.all('SELECT * FROM models', (err, rows) => {
+		db[verb](query, params, (err, result) => {
 			if (err) {
 				reject(err);
 			} else {
-				resolve(rows);
+				resolve(result);
 			}
 		});
 	});
+}
+
+export async function getAllModels() {
+	return runQuery('all', 'SELECT * FROM models');
 }
 
 export async function getModel(idName) {
-	return new Promise((resolve, reject) => {
-		db.get('SELECT * FROM models WHERE idname = ? COLLATE NOCASE', [idName], (err, row) => {
-			if (err) {
-				reject(err);
-			} else {
-				resolve(row);
-			}
-		});
-	});
+	return runQuery('get', 'SELECT * FROM models WHERE idname = ? COLLATE NOCASE', [idName]);
 }
 
 export async function getRandomModel() {
-	return new Promise((resolve, reject) => {
-		db.get('SELECT * FROM models ORDER BY RANDOM() LIMIT 1', (err, row) => {
-			if (err) {
-				reject(err);
-			} else {
-				resolve(row);
-			}
-		});
-	});
+	return runQuery('get', 'SELECT * FROM models ORDER BY RANDOM() LIMIT 1');
 }
 
 export async function updateField(idName, field, value) {
-	return new Promise((resolve, reject) => {
-		db.run(`UPDATE models SET ${field} = ? WHERE idname = ? COLLATE NOCASE`, [value, idName], (err) => {
-			if (err) {
-				reject(err);
-			} else {
-				resolve();
-			}
-		});
-	});
+	return runQuery('run', `UPDATE models SET ${field} = ? WHERE idname = ? COLLATE NOCASE`, [value, idName]);
 }
 
 export async function deleteModel(idName) {
-	return new Promise((resolve, reject) => {
-		db.run('DELETE FROM models WHERE idname = ? COLLATE NOCASE', [idName], (err) => {
-			if (err) {
-				reject(err);
-			} else {
-				resolve();
-			}
-		});
-	});
+	return runQuery('run', 'DELETE FROM models WHERE idname = ? COLLATE NOCASE', [idName]);
 }
 
 export async function addModel(idName, displayName, prompt, owner, profile) {
-	return new Promise((resolve, reject) => {
-		db.run('INSERT INTO models (idname, displayname, model, owner, profile) VALUES (?, ?, ?, ?, ?)', [idName, displayName, prompt, owner, profile], (err) => {
-			if (err) {
-				reject(err);
-			} else {
-				resolve();
-			}
-		});
-	});
+	return runQuery('run', 'INSERT INTO models (idname, displayname, model, owner, profile) VALUES (?, ?, ?, ?, ?)', [idName, displayName, prompt, owner, profile]);
 }
